67430049d00592f92dffa8dd31bba3adc5f71966
ace-suyash <ace.suyash07@gmail.com>
2026-04-18 19:52:06 +0530

    Utils setup

---
+++ src/utils/ApiResponse.js
@@ -0,0 +1,15 @@
+class ApiResponse {
+    constructor(statusCode , data, message = "Success"){
+        this.statusCode = statusCode
+        this.data = data
+        this.message = message
+        this.success = statusCode < 400
+    }
+}
+
+//Standard rules
+//informational responses (Error :  100 - 199)
+//Successful responses (Error 200 - 299)
+//Redirection messages (Error : 300 - 399)
+//Client error responses (Error : 400 - 499)
+//Server error responses (Error : 500 - 599)
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,27 @@
+const asyncHandler = (requestHandler) => {
+    (req,res,next) => {
+        Promise.resolve(requestHandler).catch((err) => next(err)).
+        catch((err) => next(err))
+    }
+}
+
+
+export {asyncHandler}
+
+// const asyncHandler = () => {}
+// const asyncHandler = (func) => () => {}
+// const ayncHandler = (func) => { () => {} }
+
+/*    
+const asyncHandler = (fn) => async (req,res,next) => {
+    try{
+        await fn(req,res,next)
+    }
+    catch (error){
+        res.status(error.code || 500).json({
+            success: false,
+            message: error.message
+        })
+    }
+}
+*/
